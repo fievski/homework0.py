b668804972f30f1ccda6b4c742a4d55df8070973
fievski <fievski@yandex.ru>
2024-06-04 23:56:57 +0600

    Add files via upload

---
+++ Фиевски. Базовые структуры данных.py
@@ -0,0 +1,27 @@
+a = "Произвольная строка"   # Задача «Длина слова».
+print (len (a))
+
+first = 5  # Задача «Суммы и разности».
+second = 4
+summa = (first + second)
+diff = (first - second)
+print (summa)
+print (diff)
+
+first = 6  # Задача «Среднее арифметическое».
+second = 9
+third = 4
+mean = (first + second + third) // 3
+print (mean)
+
+first_string = "Вторник"  # Задача «Простые строчки».
+second_string = "Понедельник"
+print (second_string + ', ' + first_string)
+
+a = 5  # Задача «Сложная формула».
+b = 10
+c = 15
+f = (a * b) + (a * c)
+print((f ** 3) / 2)
+
+# Я разобрался!
